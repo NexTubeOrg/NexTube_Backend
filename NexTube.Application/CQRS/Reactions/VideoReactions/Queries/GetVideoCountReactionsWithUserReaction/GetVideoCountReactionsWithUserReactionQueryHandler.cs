1c6006b4934b1dc62276d38b30c77419cbe9337c
Devloner <tminecraftben@gmail.com>
2023-11-30 20:41:25 +0200

    get video reactions with current user status

---
+++ NexTube.Application/CQRS/Reactions/VideoReactions/Queries/GetVideoCountReactionsWithUserReaction/GetVideoCountReactionsWithUserReactionQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using Ardalis.GuardClauses;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NexTube.Application.Common.DbContexts;
+using NexTube.Application.CQRS.Reactions.VideoReactions.Queries.GetVideoCountReactions;
+using NexTube.Application.CQRS.Reactions.VideoReactions.Queries.GetVideoUserReaction;
+
+namespace NexTube.Application.CQRS.Reactions.VideoReactions.Queries.GetVideoCountReactionsWithUserReaction {
+    public class GetVideoCountReactionsWithUserReactionQueryHandler : IRequestHandler<GetVideoCountReactionsWithUserReactionQuery, VideoReactionsCountWithStatus> {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMediator _mediator;
+
+        public GetVideoCountReactionsWithUserReactionQueryHandler(IApplicationDbContext dbContext, IMediator mediator) {
+            _dbContext = dbContext;
+            _mediator = mediator;
+        }
+
+        public async Task<VideoReactionsCountWithStatus> Handle(GetVideoCountReactionsWithUserReactionQuery request, CancellationToken cancellationToken) {
+            var result = new VideoReactionsCountWithStatus();
+
+            var videoReactionsCount = await _mediator.Send(new GetVideoCountReactionsQuery() {
+                VideoId = request.VideoId,
+            });
+            result.Likes = videoReactionsCount.Likes;
+            result.Dislikes = videoReactionsCount.Dislikes;
+
+            // get current user reaction status
+            if (request.RequesterId is not null) {
+                var getUserReactionQuery = new GetVideoUserReactionQuery() {
+                    UserId = request.RequesterId.Value,
+                    VideoId = request.VideoId
+                };
+
+                try {
+                    var reactions = await _mediator.Send(getUserReactionQuery);
+                    result.WasLikedByRequester = reactions?.ReactionType == Domain.Entities.VideoReactionEntity.VideoReactionType.Like;
+                    result.WasDislikedByRequester = reactions?.ReactionType == Domain.Entities.VideoReactionEntity.VideoReactionType.Dislike;
+                }
+                catch (NotFoundException) {
+                    // user did not reacted to the video
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- NexTube.WebApi/Controllers/VideoReactionController.cs
+++ NexTube.WebApi/Controllers/VideoReactionController.cs
@@ -13,6 +13,7 @@ using NexTube.Domain.Entities;
 using NexTube.WebApi.DTO.Reactions.VideoReactions;
 using NexTube.Application.CQRS.Reactions.VideoReactions.Queries.GetVideoUserReaction;
 using NexTube.Application.CQRS.Reactions.VideoReactions.Queries.GetVideoCountReactions;
+using NexTube.Application.CQRS.Reactions.VideoReactions.Queries.GetVideoCountReactionsWithUserReaction;
 
 namespace NexTube.WebApi.Controllers {
     [Route("api/Video/Reaction/[action]")]
@@ -50,7 +51,18 @@ namespace NexTube.WebApi.Controllers {
         [HttpGet("{videoId}")]
         public async Task<ActionResult> GetCountVideoReactions([FromRoute] int videoId) {
             var query = new GetVideoCountReactionsQuery() {
-                VideoId = videoId
+                VideoId = videoId,
+            };
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("{videoId}")]
+        [Authorize(Roles = Roles.User)]
+        public async Task<ActionResult> GetCountVideoReactionsWithStatus([FromRoute] int videoId) {
+            var query = new GetVideoCountReactionsWithUserReactionQuery() {
+                VideoId = videoId,
+                RequesterId = UserId
             };
             var result = await Mediator.Send(query);
             return Ok(result);
