47d3a39c42d8c08ba4f8098f98a3825d1e32ccf3
Devloner <tminecraftben@gmail.com>
2023-12-06 18:39:43 +0200

    get playlists by user id

---
+++ NexTube.Application/CQRS/Playlists/VideoPlaylists/Queries/GetUserPlaylists/GetCommentsListQueryValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace NexTube.Application.CQRS.Playlists.VideoPlaylists.Queries.GetUserPlaylists {
+    public class GetUserPlaylistsQueryValidation : AbstractValidator<GetUserPlaylistsQuery> {
+        public GetUserPlaylistsQueryValidation() {
+            RuleFor(c => c.Page)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(c => c.PageSize)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ NexTube.WebApi/Controllers/VideoPlaylistController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using NexTube.Application.CQRS.Playlists.VideoPlaylists.Queries.GetUserPlaylists;
+using NexTube.WebApi.DTO.Playlists;
+
+namespace NexTube.WebApi.Controllers {
+    [Route("api/Video/Playlist/[action]")]
+    public class VideoPlaylistController : BaseController {
+        private readonly IMapper mapper;
+        private readonly IMediator mediator;
+
+        public VideoPlaylistController(IMapper mapper, IMediator mediator) {
+            this.mapper = mapper;
+            this.mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetUserPlaylists([FromQuery] GetUserPlaylistsDto dto) {
+            var query = mapper.Map<GetUserPlaylistsQuery>(dto);
+            var result = await mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
