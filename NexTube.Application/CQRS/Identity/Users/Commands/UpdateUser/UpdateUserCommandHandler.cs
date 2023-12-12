4bd717ad36c7f456b5bd063af3ee375c96421816
Anonym xxx <oleksandrsoltis365@gmail.com>
2023-12-12 23:08:42 +0200

    transfer editing

--- NexTube.Application/CQRS/Identity/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ NexTube.Application/CQRS/Identity/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -1,22 +1,38 @@
-﻿using MediatR;
+﻿using Ardalis.GuardClauses;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
 using NexTube.Application.Common.Interfaces;
+using NexTube.Domain.Entities;
 
 namespace NexTube.Application.CQRS.Identity.Users.Commands.UpdateUser
 {
     public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, int>
     {
-        private readonly IIdentityService _ichenalServise;
-        public UpdateUserCommandHandler(IIdentityService ichenalServise)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UpdateUserCommandHandler(UserManager<ApplicationUser> userManager)
         {
-            _ichenalServise = ichenalServise;
+        _userManager = userManager;
         }
 
         public async Task<int> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
         {
 
-            var result = await _ichenalServise.UdateUserAsync(request.UserId, request.Nickname, request.Description, request.FirstName, request.LastName);
+            var user = await _userManager.FindByIdAsync(request.UserId.ToString());
 
-            return result.UserId;
+            if (user == null)
+            {
+                throw new NotFoundException("User", request.UserId.ToString());
+            }
+            user.Id = request.UserId;
+            user.FirstName = request.FirstName;
+            user.LastName = request.LastName;
+            user.Nickname = request.Nickname;
+            user.Description = request.Description;
+
+
+          await _userManager.UpdateAsync(user);
+          
+            return user.Id;
         }
     }
 
