f5bc2a5f4981994b1aec91bebf6c649d965efe33
Anonym xxx <oleksandrsoltis365@gmail.com>
2023-12-20 17:13:38 +0200

    Result ADD and Delete return

--- NexTube.Application/CQRS/SubscriptionUser/AddSubscriptionUser/AddSubscriptionUserCommand.cs
+++ NexTube.Application/CQRS/SubscriptionUser/AddSubscriptionUser/AddSubscriptionUserCommand.cs
@@ -1,10 +1,11 @@
 ﻿
 using MediatR;
+using NexTube.Application.CQRS.Identity.Users.Commands.AddSubscriptionUser;
 using NexTube.Domain.Entities;
 
 namespace NexTube.Application.CQRS.SubscriptionUser.AddSubscriptionUser
 {
-    public class AddSubscriptionUserCommand : IRequest<bool>
+    public class AddSubscriptionUserCommand : IRequest<AddSubscriptionUserCommandResult>
     {
         public int User { get; set; }
         public int Subscriber { get; set; }
