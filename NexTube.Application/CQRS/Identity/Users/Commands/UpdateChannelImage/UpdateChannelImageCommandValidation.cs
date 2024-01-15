123f58d9c11b7064b4a9ef17e0d982a44a5ed9b2
Anonym xxx <oleksandrsoltis365@gmail.com>
2024-01-15 19:03:18 +0200

    validation update image user

---
+++ NexTube.Application/CQRS/Identity/Users/Commands/UpdateChannelImage/UpdateChannelImageCommandValidation.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using NexTube.Application.CQRS.Identity.Users.Commands.UpdateChannelImage;
+using NexTube.Application.CQRS.Videos.Commands.UpdateVideo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexTube.Application.CQRS.Identity.Users.Commands.UpdateChannelImage
+{
+    public class UpdateChannelImageCommandValidation : AbstractValidator<UpdateChannelImageCommand>
+    {
+        public UpdateChannelImageCommandValidation() {
+
+            RuleFor(x => x.UserId)
+                   .NotEmpty().WithMessage("User ID cannot be empty");
+
+            RuleFor(x => x.ChannelPhotoFile)
+                   .NotNull().WithMessage("Channel Photo File  cannot be empty");
+
+
+
+
+
+        }
+
+
+    }
+}
