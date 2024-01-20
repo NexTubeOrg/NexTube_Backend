5ced90c312d768cdc765ca697b1f79ef8cbeb703
QusDev <qusdeveloper@gmail.com>
2024-01-20 16:22:32 +0200

    create user video history table and implement logic

---
+++ NexTube.Domain/Entities/UserVideoHistoryEntity.cs
@@ -0,0 +1,14 @@
+﻿
+namespace NexTube.Domain.Entities
+{
+    public class UserVideoHistoryEntity
+    {
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+
+        public int VideoId { get; set; }
+        public VideoEntity Video { get; set; } = null!;
+        
+        public DateTime DateWatched { get; set; }
+    }
+}
---
+++ NexTube.Persistence/Data/Configurations/History/UserVideoHistoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NexTube.Domain.Entities;
+
+namespace NexTube.Persistence.Data.Configurations.History
+{
+    public class UserVideoHistoryConfiguration : IEntityTypeConfiguration<UserVideoHistoryEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserVideoHistoryEntity> builder)
+        {
+            builder.HasKey(x => new
+            {
+                x.UserId,
+                x.VideoId
+            });
+
+            builder
+                .HasOne(h => h.User)
+                .WithMany()
+                .HasForeignKey(h => h.UserId);
+
+            builder.HasOne(h => h.Video)
+                .WithMany()
+                .HasForeignKey(h => h.VideoId);
+        }
+    }
+}
